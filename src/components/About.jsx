cbf9a67a591bf6c71732e904bda53fd695ed8461
Isabella <isabellaafry@gmail.com>
2019-05-17 07:41:45 +0100

    fsdf

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,11 @@ const About = () => {
             <h1>About the Creator</h1>
             <img className="profileImage" src="https://res.cloudinary.com/dl2ribpco/image/upload/v1557245379/ChaseTheSpace/linkedin_ekkfus.jpg" alt="" />
             <div className="textbox">
-            The creator of this app was immensely sleep deprived during its creation, and under a RIDICULOUS time constraint, so please be kind when evaluating the essence of this game. She hopes you can brush up on your math skills, and have fun!
+            The creator of this app was immensely sleep deprived during its creation, and under a RIDICULOUS time constraint, so please be kind when evaluating the essence of this game. She hopes you can brush up on your math skills, and have fun! - Isabella Fry
+                <div className="">
+                    <a href="https://www.linkedin.com/in/isabella-fry-5a1618182/" rel="noopener noreferrer" target="_blank" ><img className="iconspecs" src="https://cdn3.iconfinder.com/data/icons/sociocons/256/linkedin-sociocon.png" alt=""/></a>
+                    <a href="https://github.com/dragonslayer77" rel="noopener noreferrer" target="_blank"><img className="iconspecs" src="https://image.flaticon.com/icons/png/128/25/25231.png" alt="" /></a>
+                </div>
             </div>
             <div>
             <Link to='/'>
